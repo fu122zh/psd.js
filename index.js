2f5113d6e76b42cec3a1bc616dafe5455ef0fbe4
zengdefu <zengdefu@maka.im>
2016-08-03 23:08:07 +0800

    修改index，需要webpack配置coffee-loader

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-require('coffee-script/register');
-module.exports = require('./lib/psd.coffee');
+png = require('./shims/png.coffee');
+init = require('./shims/init.coffee');
+psd = require('./lib/psd.coffee');
+
+extended = init.extended(psd);
+
+psd.prototype.toBase64 = png.toBase64;
+
+psd.fromDroppedFile = init.fromDroppedFile;
+psd.fromEvent = init.fromEvent;
+psd.fromURL = init.fromURL;
+
+module.exports = psd;
